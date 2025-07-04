95e0a04edb3189651d1b574886f0f4842342b417
andrianfaa <andrianfadhilla@gmail.com>
2025-07-04 09:20:32 +0700

    fix: simplify port assignment logic in start.ts

--- start.ts
+++ start.ts
@@ -1,6 +1,5 @@
 import server from "./api";
 
-const port =
-    process.env.PORT || process.env.NODE_ENV === "development" ? 5000 : 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
